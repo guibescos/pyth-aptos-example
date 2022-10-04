141757712c60f41d7a998b1067369aaa4e073b33
Guillermo Bescos <gbescos@stanford.edu>
2022-10-04 05:23:30 +0000

    Fix app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,22 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const transaction = {
+  type: "entry_function_payload",
+  function: `0xb138581594ebd7763cfa3c3e455050139b7304c6d41e7094a1c78da4e6761ed8::minting::mint_nft`,
+  arguments: [],
+  type_arguments: [],
+};
+
 function App() {
+
+  const [isConnected, setIsConnected] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    window.aptos.disconnect()
+  }, []);
+
+  
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Mint your Pythian NFT 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <button onClick={async ()=> {setIsConnected(true); await window.aptos.connect(); await window.aptos.isConnected()}} disabled={isConnected}> Connect </button>
+          <button onClick={async ()=> {setIsConnected(false); await window.aptos.disconnect()}} disabled={!isConnected}> Disconnect </button>
+    <button onClick={async ()=> {await window.aptos.signAndSubmitTransaction(transaction)}} disabled={!isConnected}> Mint </button> </div>
       </header>
     </div>
   );
