a3bc4ce734f5a1e104c2d1bd0185a06ec2372231
Guillermo Bescos <gbescos@stanford.edu>
2022-10-05 18:29:40 +0000

    Add fetcher for onChain price

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,23 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { PriceServiceConnection, PriceFeed, HexString } from "@pythnetwork/pyth-common-js";
-import {AptosPriceServiceConnection} from "@pythnetwork/pyth-aptos-js"
+import {Buffer} from "buffer";
+import {AptosClient} from "aptos"
+
+class AptosPriceServiceConnection extends PriceServiceConnection {
+  /**
+   * Gets price update data which then can be submitted to the Pyth contract to update the prices.
+   * This will throw an axios error if there is a network problem or the price service returns a non-ok response (e.g: Invalid price ids)
+   *
+   * @param priceIds Array of hex-encoded price ids.
+   * @returns Array of price update data, serialized such that it can be passed to the Pyth Aptos contract.
+   */
+  async getPriceFeedsUpdateData(priceIds: HexString[]): Promise<number[][]> {
+    // Fetch the latest price feed update VAAs from the price service
+    const latestVaas : string[]= await this.getLatestVaas(priceIds);
+    return latestVaas.map((vaa) => {return Buffer.from(vaa, "base64").toJSON().data})
+  }
+}
 
 const mintTransaction = {
   type: "entry_function_payload",
@@ -19,6 +35,12 @@ const ETH_USD_MAINNET =
   "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace";
 const ETH_USD_TESTNET = "0xca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6";
 const PYTH_CONTRACT_TESTNET = "0xaa706d631cde8c634fe1876b0c93e4dec69d0c6ccac30a734e9e257042e81541";
+const APTOS_TESTNET_RPC = "https://testnet.aptoslabs.com/";
+const PYTH_TABLE_HANDLE = "0x21b2122f77d3f9f944456c0ca8ffa6a13c541476433e64ab6ae81d48277a1181";
+const aptosClient = new AptosClient(APTOS_TESTNET_RPC);
+
+
+
 
 function App() {
   const [isConnected, setIsConnected] = React.useState<boolean>(false);
@@ -26,10 +48,25 @@ function App() {
     window.aptos.disconnect();
   }, []);
 
-  const [onChainPrice, setOnChainPrice] = React.useState<number>(0);
+  const [pythOffChainPrice, setPythOffChainPrice] = React.useState<number>(0);
+  const [pythOnChainPrice, setPythOnChainPrice] = React.useState<number>(0);
+
+  const fetchOnChainPrice = async () => {
+    let data = await aptosClient.getTableItem(PYTH_TABLE_HANDLE, {
+      "key_type": "0xaa706d631cde8c634fe1876b0c93e4dec69d0c6ccac30a734e9e257042e81541::price_identifier::PriceIdentifier",
+      "value_type": "0xaa706d631cde8c634fe1876b0c93e4dec69d0c6ccac30a734e9e257042e81541::price_info::PriceInfo",
+      "key": { "bytes" : "0xca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6"}
+    });
+    console.log(data);
+    setPythOnChainPrice(data.price_feed.price.price.magnitude * 10 **(- data.price_feed.price.expo.magnitude))
+  }
+
+  React.useEffect(() => {
+    fetchOnChainPrice();
+  }, []);
 
   mainnetConnection.subscribePriceFeedUpdates([ETH_USD_MAINNET], (priceFeed: PriceFeed) => {
-    setOnChainPrice(
+    setPythOffChainPrice(
       priceFeed.getCurrentPrice()?.getPriceAsNumberUnchecked() || 0
     );
   });
@@ -39,9 +76,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Mint your Pythian NFT</p>
-        <p>Current offchain ETH/USD : {onChainPrice.toFixed(3)}</p>
-        <p>Current NFT price : {(100/onChainPrice).toFixed(5)} APT</p>
-        <p>Onchain ETH/USD :</p>
+        <p>Current offchain ETH/USD : {pythOffChainPrice.toFixed(3)}</p>
+        <p>Current NFT price : {(100/pythOffChainPrice).toFixed(5)} APT</p>
+        <p>Onchain ETH/USD : {pythOnChainPrice.toFixed(3)}</p>
         <div>
           <button
             onClick={async () => {
